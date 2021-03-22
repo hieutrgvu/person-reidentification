fd4bfe9745f7f8fb01f501cbe223a04358eb418f
Hieu Vu <hieu.vu@trustingsocial.com>
2021-03-22 15:17:12 +0700

    Remove unused model

--- model.py
+++ model.py
@@ -1,11 +1,8 @@
-import torch
 import torch.nn as nn
 from torch.nn import init
 from torchvision import models
-from torch.autograd import Variable
-import pretrainedmodels
 
-######################################################################
+
 def weights_init_kaiming(m):
     classname = m.__class__.__name__
     # print(classname)
@@ -18,11 +15,13 @@ def weights_init_kaiming(m):
         init.normal_(m.weight.data, 1.0, 0.02)
         init.constant_(m.bias.data, 0.0)
 
+
 def weights_init_classifier(m):
     classname = m.__class__.__name__
     if classname.find('Linear') != -1:
         init.normal_(m.weight.data, std=0.001)
         init.constant_(m.bias.data, 0.0)
+
 
 # Defines the new fc layer and classification layer
 # |--Linear--|--bn--|--relu--|--Linear--|
@@ -51,6 +50,7 @@ class ClassBlock(nn.Module):
 
         self.add_block = add_block
         self.classifier = classifier
+
     def forward(self, x):
         x = self.add_block(x)
         if self.return_f:
@@ -61,105 +61,6 @@ class ClassBlock(nn.Module):
             x = self.classifier(x)
             return x
 
-# Define the ResNet50-based Model
-class ft_net(nn.Module):
-
-    def __init__(self, class_num, droprate=0.5, stride=2, circle=False):
-        super(ft_net, self).__init__()
-        model_ft = models.resnet50(pretrained=True)
-        # avg pooling to global pooling
-        if stride == 1:
-            model_ft.layer4[0].downsample[0].stride = (1,1)
-            model_ft.layer4[0].conv2.stride = (1,1)
-        model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.model = model_ft
-        self.circle = circle
-        self.classifier = ClassBlock(2048, class_num, droprate, return_f = circle)
-
-    def forward(self, x):
-        x = self.model.conv1(x)
-        x = self.model.bn1(x)
-        x = self.model.relu(x)
-        x = self.model.maxpool(x)
-        x = self.model.layer1(x)
-        x = self.model.layer2(x)
-        x = self.model.layer3(x)
-        x = self.model.layer4(x)
-        x = self.model.avgpool(x)
-        x = x.view(x.size(0), x.size(1))
-        x = self.classifier(x)
-        return x
-
-# Define the DenseNet121-based Model
-class ft_net_dense(nn.Module):
-
-    def __init__(self, class_num, droprate=0.5, circle=False):
-        super().__init__()
-        model_ft = models.densenet121(pretrained=True)
-        model_ft.features.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        model_ft.fc = nn.Sequential()
-        self.model = model_ft
-        self.circle = circle
-        # For DenseNet, the feature dim is 1024 
-        self.classifier = ClassBlock(1024, class_num, droprate, return_f=circle)
-
-    def forward(self, x):
-        x = self.model.features(x)
-        x = x.view(x.size(0), x.size(1))
-        x = self.classifier(x)
-        return x
-
-# Define the NAS-based Model
-class ft_net_NAS(nn.Module):
-
-    def __init__(self, class_num, droprate=0.5):
-        super().__init__()  
-        model_name = 'nasnetalarge' 
-        # pip install pretrainedmodels
-        model_ft = pretrainedmodels.__dict__[model_name](num_classes=1000, pretrained='imagenet')
-        model_ft.avg_pool = nn.AdaptiveAvgPool2d((1,1))
-        model_ft.dropout = nn.Sequential()
-        model_ft.last_linear = nn.Sequential()
-        self.model = model_ft
-        # For DenseNet, the feature dim is 4032
-        self.classifier = ClassBlock(4032, class_num, droprate)
-
-    def forward(self, x):
-        x = self.model.features(x)
-        x = self.model.avg_pool(x)
-        x = x.view(x.size(0), x.size(1))
-        x = self.classifier(x)
-        return x
-    
-# Define the ResNet50-based Model (Middle-Concat)
-# In the spirit of "The Devil is in the Middle: Exploiting Mid-level Representations for Cross-Domain Instance Matching." Yu, Qian, et al. arXiv:1711.08106 (2017).
-class ft_net_middle(nn.Module):
-
-    def __init__(self, class_num, droprate=0.5):
-        super(ft_net_middle, self).__init__()
-        model_ft = models.resnet50(pretrained=True)
-        # avg pooling to global pooling
-        model_ft.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.model = model_ft
-        self.classifier = ClassBlock(2048+1024, class_num, droprate)
-
-    def forward(self, x):
-        x = self.model.conv1(x)
-        x = self.model.bn1(x)
-        x = self.model.relu(x)
-        x = self.model.maxpool(x)
-        x = self.model.layer1(x)
-        x = self.model.layer2(x)
-        x = self.model.layer3(x)
-        # x0  n*1024*1*1
-        x0 = self.model.avgpool(x)
-        x = self.model.layer4(x)
-        # x1  n*2048*1*1
-        x1 = self.model.avgpool(x)
-        x = torch.cat((x0,x1),1)
-        x = x.view(x.size(0), x.size(1))
-        x = self.classifier(x)
-        return x
 
 # Part Model proposed in Yifan Sun etal. (2018)
 class PCB(nn.Module):
@@ -237,13 +138,13 @@ class PCB_test(nn.Module):
 # Run this code with:
 python model.py
 '''
-if __name__ == '__main__':
-# Here I left a simple forward function.
-# Test the model, before you train it. 
-    net = ft_net(751, stride=1)
-    net.classifier = nn.Sequential()
-    print(net)
-    input = Variable(torch.FloatTensor(8, 3, 256, 128))
-    output = net(input)
-    print('net output size:')
-    print(output.shape)
+# if __name__ == '__main__':
+# # Here I left a simple forward function.
+# # Test the model, before you train it.
+#     net = ft_net(751, stride=1)
+#     net.classifier = nn.Sequential()
+#     print(net)
+#     input = Variable(torch.FloatTensor(8, 3, 256, 128))
+#     output = net(input)
+#     print('net output size:')
+#     print(output.shape)
