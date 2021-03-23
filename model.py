659608eb8f70ca64c900ed8f98d529b08098cdba
Hieu Vu <hieu.vu@trustingsocial.com>
2021-03-23 11:53:46 +0700

    Clean model

--- model.py
+++ model.py
@@ -1,13 +1,14 @@
+import torch
 import torch.nn as nn
 from torch.nn import init
 from torchvision import models
+from torch.autograd import Variable
 
 
 def weights_init_kaiming(m):
     classname = m.__class__.__name__
-    # print(classname)
     if classname.find('Conv') != -1:
-        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in') # For old pytorch, you may use kaiming_normal.
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
     elif classname.find('Linear') != -1:
         init.kaiming_normal_(m.weight.data, a=0, mode='fan_out')
         init.constant_(m.bias.data, 0.0)
@@ -26,125 +27,80 @@ def weights_init_classifier(m):
 # Defines the new fc layer and classification layer
 # |--Linear--|--bn--|--relu--|--Linear--|
 class ClassBlock(nn.Module):
-    def __init__(self, input_dim, class_num, droprate, relu=False, bnorm=True, num_bottleneck=512, linear=True, return_f = False):
+    def __init__(self, input_dim, class_num, drop_rate, batch_norm=True, num_bottleneck=256, linear=True):
         super(ClassBlock, self).__init__()
-        self.return_f = return_f
         add_block = []
         if linear:
             add_block += [nn.Linear(input_dim, num_bottleneck)]
         else:
             num_bottleneck = input_dim
-        if bnorm:
+        if batch_norm:
             add_block += [nn.BatchNorm1d(num_bottleneck)]
-        if relu:
-            add_block += [nn.LeakyReLU(0.1)]
-        if droprate>0:
-            add_block += [nn.Dropout(p=droprate)]
+        if drop_rate > 0:
+            add_block += [nn.Dropout(p=drop_rate)]
         add_block = nn.Sequential(*add_block)
         add_block.apply(weights_init_kaiming)
+        self.add_block = add_block
 
-        classifier = []
-        classifier += [nn.Linear(num_bottleneck, class_num)]
+        classifier = [nn.Linear(num_bottleneck, class_num)]
         classifier = nn.Sequential(*classifier)
         classifier.apply(weights_init_classifier)
-
-        self.add_block = add_block
         self.classifier = classifier
 
     def forward(self, x):
         x = self.add_block(x)
-        if self.return_f:
-            f = x
-            x = self.classifier(x)
-            return [x,f]
-        else:
-            x = self.classifier(x)
-            return x
+        return self.classifier(x)
 
 
-# Part Model proposed in Yifan Sun etal. (2018)
 class PCB(nn.Module):
     def __init__(self, class_num ):
         super(PCB, self).__init__()
 
-        self.part = 6 # We cut the pool5 to 6 parts
-        model_ft = models.resnet50(pretrained=True)
-        self.model = model_ft
-        self.avgpool = nn.AdaptiveAvgPool2d((self.part,1))
+        self.part = 6
+        self.model = models.resnet50(pretrained=True)
+        self.avgpool = nn.AdaptiveAvgPool2d((self.part, 1))
         self.dropout = nn.Dropout(p=0.5)
+
         # remove the final downsample
-        self.model.layer4[0].downsample[0].stride = (1,1)
-        self.model.layer4[0].conv2.stride = (1,1)
-        # define 6 classifiers
+        self.model.layer4[0].downsample[0].stride = (1, 1)
+        self.model.layer4[0].conv2.stride = (1, 1)
+
+        # define classifier for each part
         for i in range(self.part):
-            name = 'classifier'+str(i)
-            setattr(self, name, ClassBlock(2048, class_num, droprate=0.5, relu=False, bnorm=True, num_bottleneck=256))
+            name = 'classifier' + str(i)
+            setattr(self, name, ClassBlock(2048, class_num, drop_rate=0.5))
 
     def forward(self, x):
         x = self.model.conv1(x)
         x = self.model.bn1(x)
         x = self.model.relu(x)
         x = self.model.maxpool(x)
-        
         x = self.model.layer1(x)
         x = self.model.layer2(x)
         x = self.model.layer3(x)
         x = self.model.layer4(x)
         x = self.avgpool(x)
         x = self.dropout(x)
-        part = {}
-        predict = {}
+
         # get six part feature batchsize*2048*6
+        part, predict = {}, {}
         for i in range(self.part):
-            part[i] = x[:,:,i].view(x.size(0), x.size(1))
-            name = 'classifier'+str(i)
-            c = getattr(self,name)
+            part[i] = x[:, :, i].view(x.size(0), x.size(1))
+            name = 'classifier' + str(i)
+            c = getattr(self, name)
             predict[i] = c(part[i])
 
-        # sum prediction
-        #y = predict[0]
-        #for i in range(self.part-1):
-        #    y += predict[i+1]
-        y = []
-        for i in range(self.part):
-            y.append(predict[i])
-        return y
+        return list(predict.values())
 
-class PCB_test(nn.Module):
-    def __init__(self,model):
-        super(PCB_test,self).__init__()
-        self.part = 6
-        self.model = model.model
-        self.avgpool = nn.AdaptiveAvgPool2d((self.part,1))
-        # remove the final downsample
-        self.model.layer4[0].downsample[0].stride = (1,1)
-        self.model.layer4[0].conv2.stride = (1,1)
 
-    def forward(self, x):
-        x = self.model.conv1(x)
-        x = self.model.bn1(x)
-        x = self.model.relu(x)
-        x = self.model.maxpool(x)
-
-        x = self.model.layer1(x)
-        x = self.model.layer2(x)
-        x = self.model.layer3(x)
-        x = self.model.layer4(x)
-        x = self.avgpool(x)
-        y = x.view(x.size(0),x.size(1),x.size(2))
-        return y
 '''
-# debug model structure
-# Run this code with:
-python model.py
+debug model structure, run this code with:
+$ python model.py
 '''
-# if __name__ == '__main__':
-# # Here I left a simple forward function.
-# # Test the model, before you train it.
-#     net = ft_net(751, stride=1)
-#     net.classifier = nn.Sequential()
-#     print(net)
-#     input = Variable(torch.FloatTensor(8, 3, 256, 128))
-#     output = net(input)
-#     print('net output size:')
-#     print(output.shape)
+if __name__ == '__main__':
+    net = PCB(500)
+    print(net)
+    input = Variable(torch.FloatTensor(32, 3, 256, 128))
+    print('input shape:', input.shape)
+    outputs = net(input)
+    print('output shape:', len(outputs), 'parts x', outputs[0].shape)
